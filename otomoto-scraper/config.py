0f1bde12632c5362af25d664e9ed239194b41e70
Maciej Szafranski <maciej.szafranski1@gmail.com>
2026-03-31 22:03:07 +0200

    config files update

--- otomoto-scraper/config.py
+++ otomoto-scraper/config.py
@@ -7,11 +7,20 @@ DATA_DIR = PROJECT_ROOT / "data" / "otomoto"
 # URL = "https://www.otomoto.pl/osobowe/mitsubishi/asx/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000"
 # CSV_FILE = str(DATA_DIR / "mitsubishi-asx.csv")
 
+URL = "https://www.otomoto.pl/osobowe/mitsubishi/asx/od-2016?page=5&search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000"
+CSV_FILE = str(DATA_DIR / "mitsubishi-asx.csv")
+
 # URL = "https://www.otomoto.pl/osobowe/dacia/duster/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000https://www.otomoto.pl/osobowe/dacia/duster/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000https://www.otomoto.pl/osobowe/dacia/duster/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000"
 # CSV_FILE = str(DATA_DIR / "dacia-duster.csv")
 
-URL = "https://www.otomoto.pl/osobowe/kia/sportage/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol"
-CSV_FILE = str(DATA_DIR / "kia-sportage.csv")
+# URL = "https://www.otomoto.pl/osobowe/dacia/duster/od-2016?page=9&search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000"
+# CSV_FILE = str(DATA_DIR / "dacia-duster.csv")
+
+# URL = "https://www.otomoto.pl/osobowe/kia/sportage/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol"
+# CSV_FILE = str(DATA_DIR / "kia-sportage.csv")
+
+# URL = "https://www.otomoto.pl/osobowe/kia/sportage/od-2016?page=10&search%5Bfilter_enum_fuel_type%5D=petrol"
+# CSV_FILE = str(DATA_DIR / "kia-sportage.csv")
 
 HEADLESS = False
 WAIT_MS = 3000
