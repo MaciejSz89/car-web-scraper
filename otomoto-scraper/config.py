0bb0e175466ac1dd3716b527b2a37cf1fa35c607
Maciej Szafranski <maciej.szafranski1@gmail.com>
2026-03-31 21:15:52 +0200

    final fix after init

--- otomoto-scraper/config.py
+++ otomoto-scraper/config.py
@@ -1,11 +1,17 @@
-URL = "https://www.otomoto.pl/osobowe/mitsubishi/asx/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000"
-CSV_FILE = "./data/mitsubishi-asx.csv"
+from pathlib import Path
+
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATA_DIR = PROJECT_ROOT / "data" / "otomoto"
+
+# URL = "https://www.otomoto.pl/osobowe/mitsubishi/asx/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000"
+# CSV_FILE = str(DATA_DIR / "mitsubishi-asx.csv")
 
 # URL = "https://www.otomoto.pl/osobowe/dacia/duster/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000https://www.otomoto.pl/osobowe/dacia/duster/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000https://www.otomoto.pl/osobowe/dacia/duster/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol&search%5Bfilter_float_mileage%3Ato%5D=150000"
-# CSV_FILE = "./data/dacia-duster.csv"
+# CSV_FILE = str(DATA_DIR / "dacia-duster.csv")
 
-# URL = "https://www.otomoto.pl/osobowe/kia/sportage/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol"
-# CSV_FILE = "./data/kia-sportage.csv"
+URL = "https://www.otomoto.pl/osobowe/kia/sportage/od-2016?search%5Bfilter_enum_fuel_type%5D=petrol"
+CSV_FILE = str(DATA_DIR / "kia-sportage.csv")
 
 HEADLESS = False
 WAIT_MS = 3000
